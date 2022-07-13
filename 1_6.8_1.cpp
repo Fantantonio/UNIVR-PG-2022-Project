b38f3e949bcc51148f99e5730205a6fa201b03fc
Fantantonio <antoniopanfili@gmail.com>
2022-07-13 18:52:09 +0200

    Add esercitazioni shaders

---
+++ 1_6.8_1.cpp
@@ -0,0 +1,133 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <../../Project1/Project1/1_6.8_1_shader.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// impostazioni finestra
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+int main()
+{
+    // glfw: inizializzazione e configurazione
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw: creazione finestra
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: carica tutti i puntatori alle funzioni OpenGL
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // shaders
+    Shader shader("1_6.8_1_shader.vs", "1_6.8_1_shader.fs");
+
+
+
+    // inizializzazione dei vertici 
+    // primo triangolo
+    float vertices[] = {
+        // posizione         // colore
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // basso destra
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // basso sinistra
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // alto 
+    };
+
+    // VBO = vertex buffer object
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // imposta gli attributi dei vertici
+    // posizione
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // colore
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+    // decommenta per la visualizzazione di tipo wireframe.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+    //************************************
+    // CICLO DI RENDERING
+    //************************************
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        processInput(window);
+
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.5f, 1.0f); //state-setting fun
+        glClear(GL_COLOR_BUFFER_BIT); // state-using fun
+
+        // disegna il triangolo
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // glfw: scambia i buffer e interroga gli eventi IO (tasti premuti, movimento del mouse etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // de-alloco le risorse non più necessarie:
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    // glfw: termina e pulisce tutte le risorse allocate da GLFW.
+    glfwTerminate();
+    return 0;
+}
+
+
+
+
+//************************************
+// FUNZIONI
+//************************************
+
+// listener degli eventi IO gestiti poi da GLFW
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// listener del resize della finestra
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // si assicura che il viewport corrisponda alla nuova dimensione della finestra;
+    // su display a retina le dimensioni della finestra sono molto maggiori di quelle specificate
+    glViewport(0, 0, width, height);
+}
