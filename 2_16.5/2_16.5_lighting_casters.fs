3342b1eaeadae5698792839ee07848f6a9582b04
Fantantonio <antoniopanfili@gmail.com>
2022-07-16 19:29:03 +0200

    Add esercitazioni Light casters e Multiple lights

---
+++ 2_16.5/2_16.5_lighting_casters.fs
@@ -0,0 +1,70 @@
+#version 330 core
+out vec4 FragColor;
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+}; 
+
+struct Light {
+    vec3 position;
+    vec3 direction;
+    float cutOff;
+    float outerCutOff;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+  
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+void main()
+{
+    vec3 lightDir = normalize(light.position - FragPos);
+    
+    // controlla se è all'interno del cono di luce
+    float theta = dot(lightDir, normalize(-light.direction)); 
+    
+    if(theta > light.cutOff) // dato che si lavora su angoli attraverso coseni e non gradi quindi l'angolo deve essere maggiore
+    {    
+        // luce ambientale
+        vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+        
+        // luce diffusa 
+        vec3 norm = normalize(Normal);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
+        
+        // luce speculare
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);  
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
+        
+        // attenuation
+        float distance    = length(light.position - FragPos);
+        float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+
+        //ambient  *= attenuation;
+        diffuse   *= attenuation;
+        specular *= attenuation;   
+            
+        vec3 result = ambient + diffuse + specular;
+        FragColor = vec4(result, 1.0);
+    }
+    else 
+    {
+        // altrimenti usa la luce ambientale
+        FragColor = vec4(light.ambient * texture(material.diffuse, TexCoords).rgb, 1.0);
+    }
+}
